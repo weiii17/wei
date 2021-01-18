d8649a162af202395fc10068c04c77a19c0a9232
weiii17 <70867367+weiii17@users.noreply.github.com>
2021-01-18 10:32:56 +0800

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -2,61 +2,40 @@
 #include <cstdlib>
 using namespace std;
 
-class Employee
+
+class Myclass
 {
 	private:
-		
-		char name[20];
-		int age;
-		int salary;
-		char mobile[10];
-		
+	    int number;
+	    int payment;
+	    
 	public:
-		
-		void input_data()
-		{
-			cout<<"name:";
-			cin>>name;
-			cout<<"age:";
-			cin>>age;
-			cout<<"salary:";
-			cin>>salary;
-			cout<<"mobile:";
-			cin>>mobile;
-		}
+	   Myclass();
+	   Myclass (int h,int m)
+	   {
+	   	   number = h;
+	   	   payment = m;
+		};
 	void print_data()
 	{
-		cout<<name<<endl;
-		cout<<age<<endl;
-		cout<<salary<<endl;
-		cout<<mobile<<endl;
-	}
 		
+		cout<<number<<endl;
+		cout<<payment<<endl;
+		
+	};	    
+	
 };
 
 
 
 int main()
 {
-	Employee em1;
+	Myclass c1(1001,18000);
+	c1.print_data();
 	
-	cout<<"輸入員工資料:"<<endl;
-	em1.input_data();
-	cout<<"輸出員工資料:"<<endl;
-	em1.print_data();
+	Myclass c2(1002,25000);
+	c2.print_data();
 	
-	system("pause");
+	system("Pause");
 	return 0;
 }
-
-輸入員工資料:
-name:林佳薰
-age:17
-salary:1
-mobile:0987878787
-輸出員工資料:
-林佳薰
-17
-1
-0987878787
-請按任意鍵繼續 . . .
